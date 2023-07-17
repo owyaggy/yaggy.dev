825b8ff8ee420a12a139a4fe1042dfc48eb7e823
Owen Yaggy <owyaggy@icloud.com>
2023-07-17 01:37:59 -0400

    set up files

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+app = Flask(__name__) 
+
+@app.route("/")       
+def hello_world():
+    print("the __name__ of this module is... ")
+    print(__name__)
+    return "No hablo queso!"
+
+if __name__ == "__main__":  # true if this file NOT imported
+    app.debug = True        # enable auto-reload upon code change
+    app.run()
